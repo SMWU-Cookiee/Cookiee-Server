a665385d426305eb98678ab68c6e35c3abbe0edf
Suyeon <syhhwang1231@gmail.com>
2023-10-02 01:51:09 -0500

    #2 Fix: 유저 컨트롤러에서 response 전달 방식 수정
    - 에러났을 때 전달 위해서 리턴형을 BaseResponse로 바꿈

--- cookiee-server/src/main/java/com/cookiee/cookieeserver/controller/UserController.java
+++ cookiee-server/src/main/java/com/cookiee/cookieeserver/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.cookiee.cookieeserver.controller;
 
+import com.cookiee.cookieeserver.constant.StatusCode;
 import com.cookiee.cookieeserver.domain.User;
 import com.cookiee.cookieeserver.dto.BaseResponseDto;
 import com.cookiee.cookieeserver.dto.DataResponseDto;
+import com.cookiee.cookieeserver.dto.ErrorResponseDto;
 import com.cookiee.cookieeserver.repository.UserRepository;
 import com.cookiee.cookieeserver.service.UserService;
 import jakarta.transaction.Transactional;
@@ -10,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
 @RestController
@@ -19,40 +22,60 @@ public class UserController {
     private final UserService userService;
     @Autowired
     private final UserRepository userRepository;
-//    public BaseResponse2<UserDto> getUser(@PathVariable Long userId) {
-//        UsersResponseDto usersResponseDto = UsersResponseDto.from(userRepository.findByUserId(userId)); // 유저 정보
-//        List<MyPlantResponseDto> myPlantResponseDtos = myPlantRepository.findAllByUserIdx(userId); // 내 식물 리스트
-//
-//        return new BaseResponse2<>(new UserDto(usersResponseDto, myPlantResponseDtos));
-//    }
+
     // 유저 프로필 조회
     @GetMapping("/users/{userId}")
-    public DataResponseDto<User> getUser(@PathVariable int userId){
-        User user = userRepository.findById(userId).orElseThrow(new Supplier<IllegalArgumentException>(){
-            @Override
-            public IllegalArgumentException get() {
-                return new IllegalArgumentException("해당 유저는 존재하지 않습니다. id: " + userId);
-            }
-        });
+    public BaseResponseDto<User> getUser(@PathVariable int userId){
+        Optional<User> user;
 
-        return DataResponseDto.of(user);
+//        try{
+//
+//        }
+//        catch (IllegalArgumentException e){
+//            return ErrorResponseDto.of(StatusCode.BAD_REQUEST, "해당 id의 사용자가 존재하지 않습니다.");
+//        }
+//
+//        return DataResponseDto.of(user);
+        user = userRepository.findById(userId);
+        if(user.isEmpty()){
+            return ErrorResponseDto.of(StatusCode.BAD_REQUEST, "해당 id의 사용자가 존재하지 않습니다.");
+        }
+        else{
+            return DataResponseDto.of(user);
+        }
     }
 
     // 유저 프로필 수정 (닉네임, 한줄 소개, 프로필사진)
     @Transactional
     @PutMapping("/users/{userId}")
-    public DataResponseDto<User> updateUser(@PathVariable int userId, @RequestBody User requestUser){
-        User newUser = userRepository.findById(userId).orElseThrow(new Supplier<IllegalArgumentException>() {
-            @Override
-            public IllegalArgumentException get() {
-                return new IllegalArgumentException("프로필 수정에 실패하였습니다.");
-            }
-        });
-        newUser.setNickname(requestUser.getNickname());
-        newUser.setSelfDescription(requestUser.getSelfDescription());
-        newUser.setProfileImage(requestUser.getProfileImage());
+    public BaseResponseDto<User> updateUser(@PathVariable int userId, @RequestBody User requestUser){
+//        User newUser = userRepository.findById(userId).orElseThrow(new Supplier<IllegalArgumentException>() {
+//            @Override
+//            public IllegalArgumentException get() {
+//                return new IllegalArgumentException("프로필 수정에 실패하였습니다.");
+//            }
+//        });
+//        newUser.setNickname(requestUser.getNickname());
+//        newUser.setSelfDescription(requestUser.getSelfDescription());
+//        newUser.setProfileImage(requestUser.getProfileImage());
 
-        return DataResponseDto.of(newUser, "회원 정보를 성공적으로 수정하였습니다.");
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isEmpty()){
+            return ErrorResponseDto.of(StatusCode.BAD_REQUEST, "해당 id의 사용자가 존재하지 않습니다.");
+        }
+        else{
+            try{
+                user.ifPresent(it -> {
+                    it.setNickname(requestUser.getNickname());
+                    it.setSelfDescription(requestUser.getSelfDescription());
+                    it.setProfileImage(requestUser.getProfileImage());
+                });
+                return DataResponseDto.of(user, "회원 정보를 성공적으로 수정하였습니다.");
+            }
+            catch (Exception e){
+                return ErrorResponseDto.of(StatusCode.INTERNAL_ERROR, "회원 정보 수정에 실패하였습니다.");
+            }
+        }
     }
 
     // 유저 프로필 삭제
