4ecd8221b11d98ec3d11eeda0ea5550dacf0991c
영서 <dudrhy12@sookmyung.ac.kr>
2023-12-25 04:59:16 +0900

    Feat: 이벤트 등록을 위한 findByUserId

--- cookiee-server/src/main/java/com/cookiee/cookieeserver/repository/UserRepository.java
+++ cookiee-server/src/main/java/com/cookiee/cookieeserver/repository/UserRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByUserId(Long UserId);
+    User findByUserId(Long UserId);
 
 }
