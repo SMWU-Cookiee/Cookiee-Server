da4f715b60ad203a47079f0726f8fed560d29473
영서 <dudrhy12@sookmyung.ac.kr>
2023-12-25 04:58:57 +0900

    Feat: 이벤트 등록 Service

--- cookiee-server/src/main/java/com/cookiee/cookieeserver/service/EventService.java
+++ cookiee-server/src/main/java/com/cookiee/cookieeserver/service/EventService.java
@@ -1,13 +1,20 @@
 package com.cookiee.cookieeserver.service;
 
 import com.cookiee.cookieeserver.domain.Event;
+import com.cookiee.cookieeserver.dto.request.EventRegisterRequestDto;
+import com.cookiee.cookieeserver.dto.response.EventResponseDto;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 public interface EventService {
-    Long createEvent(MultipartFile image, Event event, Long UserId) throws IOException;
+    Long createEvent(List<MultipartFile> image, MultipartFile thumbnail, EventRegisterRequestDto eventRegisterRequestDto, Long UserId) throws IOException;
 
+
+/*
     <T> Optional<T> searchEvent(long userId, long eventId) throws IOException;
+*/
 }
--- cookiee-server/src/main/java/com/cookiee/cookieeserver/service/EventServiceImpl.java
+++ cookiee-server/src/main/java/com/cookiee/cookieeserver/service/EventServiceImpl.java
@@ -1,8 +1,11 @@
 package com.cookiee.cookieeserver.service;
 
 import com.cookiee.cookieeserver.controller.S3Uploader;
+import com.cookiee.cookieeserver.domain.Category;
 import com.cookiee.cookieeserver.domain.Event;
 import com.cookiee.cookieeserver.domain.User;
+import com.cookiee.cookieeserver.dto.request.EventRegisterRequestDto;
+import com.cookiee.cookieeserver.repository.CategoryRepository;
 import com.cookiee.cookieeserver.repository.EventRepository;
 import com.cookiee.cookieeserver.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,30 +24,45 @@ public class EventServiceImpl implements EventService {
 
     private final EventRepository eventRepository;
     private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+
 
     @Autowired
     private S3Uploader s3Uploader;
 
-    public EventServiceImpl(EventRepository eventRepository, S3Uploader s3Uploader, UserRepository userRepository){
+    public EventServiceImpl(EventRepository eventRepository, S3Uploader s3Uploader, UserRepository userRepository, CategoryRepository categoryRepository){
         this.eventRepository = eventRepository;
         this.userRepository = userRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @Override @Transactional
-    public Long createEvent(MultipartFile image, Event event, Long userId) throws IOException {
-        if(!image.isEmpty()) {
-            String storedFileName = s3Uploader.saveFile(image);
-            event.setImageUrl(storedFileName);
+    public Long createEvent(List<MultipartFile> images, MultipartFile thumbnail, EventRegisterRequestDto eventRegisterRequestDto, Long userId) throws IOException {
+        User user = userRepository.findByUserId(userId);
+        List<Category> categories = categoryRepository.findAllByEventEventId(eventRegisterRequestDto.eventId());
+        if (!images.isEmpty()) {
+            List<String> storedFileNames = new ArrayList<>();
+
+            for (MultipartFile image : images) {
+                String storedFileName = s3Uploader.saveFile(image);
+                eventRegisterRequestDto.toEntity(user, categories).setImageUrl(Collections.singletonList(storedFileName));
+            }
         }
-        Optional<User> user = userRepository.findByUserId(userId);
-        Event savedEvent = eventRepository.save(event);
-        return savedEvent.getEvent_id();
+        String storedFileName1 = s3Uploader.saveFile(thumbnail);
+        eventRegisterRequestDto.toEntity(user, categories).setThumbnailUrl(storedFileName1);
+        Event savedEvent = eventRepository.save(eventRegisterRequestDto.toEntity(user, categories));
+        return savedEvent.getEventId();
     }
 
-    @Override @Transactional
+/*    @Override
+    public <T> Optional<T> searchEvent(long userId, long eventId) throws IOException {
+        return Optional.empty();
+    }*/
+
+/*    @Override @Transactional
     public Long serchEvent(Long userId, Long eventId) throws IOException{
 
 
-    }
+    }*/
 
 }
