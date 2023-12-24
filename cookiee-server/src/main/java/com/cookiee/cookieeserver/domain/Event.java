4d86a931b805503d7dc7a632104a2d0709ee441d
영서 <dudrhy12@sookmyung.ac.kr>
2023-12-25 04:59:59 +0900

    Fix: Event Entity 수정

--- cookiee-server/src/main/java/com/cookiee/cookieeserver/domain/Event.java
+++ cookiee-server/src/main/java/com/cookiee/cookieeserver/domain/Event.java
@@ -3,7 +3,6 @@ package com.cookiee.cookieeserver.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +16,7 @@ public class Event extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long event_id;  // 이벤트 PK
+    private Long eventId;  // 이벤트 PK
 
     @Column(nullable = true, length = 100)
     private String eventWhat; // 이벤트 내용
@@ -37,20 +36,23 @@ public class Event extends BaseTimeEntity {
     @Column(nullable = false)
     private int eventDate;
 
-    @Column(nullable = false)
-    private String imageUrl; //이미지 사진
+    @Column(nullable = true)
+    private List<String> imageUrl; //이미지 사진
+
+    @Column(nullable = true)
+    private String thumbnailUrl; //썸네일 사진
 
     @ManyToOne  // 다대일 단방향 관계, user 삭제되면 이벤트도 삭제
     @JoinColumn(name = "userId")
     private User user;  // 유저 pk (FK)
 
     //event : category = 1 : 다 -> 수연이쪽에 매핑해주어야 함
-    @OneToMany(mappedBy = "eventId")
-    private List<Collection> collections = new ArrayList<>();
+    @OneToMany(mappedBy = "event")
+    private List<Category> categories = new ArrayList<>();
 
     @Builder
     public Event(String eventWhat, String eventWhere, String withWho, int eventYear, int eventMonth, int eventDate,
-                 String imageUrl, User user, List<Collection> collections){
+                 List<String> imageUrl, String thumbnailUrl, User user, List<Category> categories){
         this.eventWhat = eventWhat;
         this.eventWhere = eventWhere;
         this.withWho = withWho;
@@ -58,8 +60,9 @@ public class Event extends BaseTimeEntity {
         this.eventMonth = eventMonth;
         this.eventDate = eventDate;
         this.imageUrl = imageUrl;
+        this.thumbnailUrl = thumbnailUrl;
         this.user = user;
-        this.collections = collections;
+        this.categories = categories;
 
 
     }
