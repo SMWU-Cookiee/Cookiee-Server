f49d7bd6b1fae5d056182082f373248c442cef58
youngseo cho <dudtj0412@naver.com>
2023-12-24 16:37:14 +0900

    Feat: S3 Config

--- cookiee-server/src/main/java/com/cookiee/cookieeserver/Config/S3Config.java
+++ cookiee-server/src/main/java/com/cookiee/cookieeserver/Config/S3Config.java
@@ -1,10 +1,8 @@
-/*
 package com.cookiee.cookieeserver.Config;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -12,25 +10,19 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
-    @Value("${cloud.aws.credentials.accessKey}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
-
-    @Value("${cloud.aws.credentials.secretKey}")
+    @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
-
     @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
-    public AmazonS3 amazonS3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey); //자격증명 생성
-
-        return AmazonS3ClientBuilder
-                .standard() //빌더 객체 생성
-                .withCredentials(new AWSStaticCredentialsProvider(credentials)) //자격 증명 정보 설정
-                .withRegion(region) //지역 설정
-                .build(); //최종적으로 S3 클라이언트를 생성하고 반환
-
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials awsCredentials= new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
     }
-
-}*/
+}
