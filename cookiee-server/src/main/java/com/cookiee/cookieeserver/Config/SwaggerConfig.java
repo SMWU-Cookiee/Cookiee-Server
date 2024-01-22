55d8c745ad0d84b2ae17bcf949c279e3b030a546
영서 <dudrhy12@sookmyung.ac.kr>
2024-01-23 03:12:30 +0900

    Feat: Swagger 설정

--- cookiee-server/src/main/java/com/cookiee/cookieeserver/Config/SwaggerConfig.java
+++ cookiee-server/src/main/java/com/cookiee/cookieeserver/Config/SwaggerConfig.java
@@ -2,41 +2,47 @@
 
 package com.cookiee.cookieeserver.Config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+@OpenAPIDefinition(
+        info = @Info(title = "Cookiee API 명세서",
+                description = "Cookiee API 명세서 입니다.",
+                version = "v1"))
+@RequiredArgsConstructor
 @Configuration
-@EnableSwagger2
-@EnableAutoConfiguration
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
 
-    private final String version = "v1";
+    //http://localhost:8080/swagger-ui/index.html#/
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/**"};
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Cookiee- API")
-                .description("Cookiee- API 명세서 입니다.")
+        return GroupedOpenApi.builder()
+                .group("Cookiee API v1")
+                .pathsToMatch(paths)
                 .build();
     }
 
-    @Bean
-    public Docket commonApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName(version)
-                .apiInfo(this.apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.cookiee.cookieeserver"))
-                .paths(PathSelectors.any())
-                .build();
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        // -- Static resources
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+
     }
 }
-
