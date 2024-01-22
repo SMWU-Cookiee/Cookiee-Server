1c591125dda283a9e4104c47d6117e0085a67a9b
영서 <dudrhy12@sookmyung.ac.kr>
2024-01-23 03:13:44 +0900

    Fix: 썸네일 조회 오류 수정

--- cookiee-server/src/main/java/com/cookiee/cookieeserver/repository/ThumbnailRepository.java
+++ cookiee-server/src/main/java/com/cookiee/cookieeserver/repository/ThumbnailRepository.java
@@ -2,13 +2,15 @@ package com.cookiee.cookieeserver.repository;
 
 import com.cookiee.cookieeserver.domain.Event;
 import com.cookiee.cookieeserver.domain.Thumbnail;
+import com.cookiee.cookieeserver.dto.response.ThumbnailGetResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ThumbnailRepository extends JpaRepository<Thumbnail, Long> {
-    List<Thumbnail> findByUserID(Long userId);
+    List<Thumbnail> findThumbnailsByUserUserId(@Param("userId") int userId);
 
-    Optional<Thumbnail> findByUserIDAndThumbnailId(long userId, long thumbnailId);
+    Optional<Thumbnail> findByUserUserIdAndThumbnailId(int userId, long thumbnailId);
 }
