b9c877508574710bfbec09e6c2571de105521539
영서 <dudrhy12@sookmyung.ac.kr>
2024-01-28 13:14:59 +0900

    Feat: 썸네일 수정 기능

--- cookiee-server/src/main/java/com/cookiee/cookieeserver/dto/response/ThumbnailResponseDto.java
+++ cookiee-server/src/main/java/com/cookiee/cookieeserver/dto/response/ThumbnailResponseDto.java
@@ -1,25 +1,33 @@
 package com.cookiee.cookieeserver.dto.response;
 
-import com.cookiee.cookieeserver.domain.Event;
 import com.cookiee.cookieeserver.domain.Thumbnail;
 import lombok.Builder;
 
 @Builder
-public record ThumbnailGetResponseDto(
+public record ThumbnailResponseDto(
     Long thumbnailId,
     int eventYear,
     int eventMonth,
     int eventDate,
     String thumbnailUrl){
 
-    public static ThumbnailGetResponseDto from(Thumbnail thumbnail){
-        return ThumbnailGetResponseDto.builder()
+    public static ThumbnailResponseDto from(Thumbnail thumbnail){
+        return ThumbnailResponseDto.builder()
                 .thumbnailId(thumbnail.getThumbnailId())
                 .eventYear(thumbnail.getEventYear())
                 .eventMonth(thumbnail.getEventMonth())
                 .eventDate(thumbnail.getEventDate())
                 .thumbnailUrl(thumbnail.getThumbnailUrl().toString())
                 .build();
+    }
+
+    @Builder
+    public ThumbnailResponseDto(Long thumbnailId, int eventYear, int eventMonth, int eventDate, String thumbnailUrl){
+        this.thumbnailId = thumbnailId;
+        this.eventYear = eventYear;
+        this.eventMonth = eventMonth;
+        this.eventDate = eventDate;
+        this.thumbnailUrl = thumbnailUrl;
     }
 }
 
