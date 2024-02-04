bcd017b202fd9aab2416c92ec9c4156f8e607a0f
Suyeon <syhhwang1231@gmail.com>
2024-02-05 02:17:16 +0900

    #21 Feat: 로그인 response dto

---
+++ cookiee-server/src/main/java/com/cookiee/cookieeserver/login/OAuthResponse.java
@@ -0,0 +1,15 @@
+package com.cookiee.cookieeserver.login;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class OAuthResponse {
+    private String id;  // 유저 소셜 아이디
+    private String email;  // 회원 이메일
+    private String socialType;  // 소셜 로그인 타입(구글 혹은 애플)
+    private String accessToken;  // 액세스 토큰
+    private String refreshToken;  // 리프레쉬 토큰
+    private Boolean isNewMember;  // 새로운 유저인지 아닌지
+}
