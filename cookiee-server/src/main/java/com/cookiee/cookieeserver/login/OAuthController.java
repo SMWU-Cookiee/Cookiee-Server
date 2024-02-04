e43d3b3f2bfe760773d243e1afbf359eb8246b9d
Suyeon <syhhwang1231@gmail.com>
2024-02-05 02:21:34 +0900

    #1 #2 #21 Feat: 유저 계정 관련 컨트롤러
    - 추후 회원가입, 회원탈퇴 등 다뤄야 함

---
+++ cookiee-server/src/main/java/com/cookiee/cookieeserver/login/OAuthController.java
@@ -0,0 +1,18 @@
+package com.cookiee.cookieeserver.login;
+
+import com.cookiee.cookieeserver.global.dto.BaseResponseDto;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class OAuthController {
+
+    /**
+     * 새 사용자가 소셜 로그인 후 회원 정보 입력할 때
+     * @return
+     */
+//    @PostMapping("/signup")
+//    public BaseResponseDto<?> signup(){
+//
+//    }
+
+
+}
