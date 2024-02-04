9d69aa3ef1679eb6ee78e12033164f5c7fd3a52d
Suyeon <syhhwang1231@gmail.com>
2024-02-05 02:16:12 +0900

    #21 Feat: 자주 사용되는 값 Constant로 설정

--- cookiee-server/src/main/java/com/cookiee/cookieeserver/global/Constant.java
+++ cookiee-server/src/main/java/com/cookiee/cookieeserver/global/Constant.java
@@ -3,6 +3,7 @@ package com.cookiee.cookieeserver.global;
 public class Constant {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_AUTHORIZATION = "Authorization";
+    public static final String HEADER_REFRESH_TOKEN = "RefreshToken";
     public static final String HEADER_APPLE_AUTHORIZATION_CODE = "AuthorizationCode";
     public static final String HEADER_IDENTITY_TOKEN = "IdentityToken";
 }
