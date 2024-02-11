e62e3a8b218aa5581375fcb9084d7fa09caf54cb
Suyeon <syhhwang1231@gmail.com>
2024-02-11 19:35:09 +0900

    Fix: 애플 로그인 경로 수정 및 예외 처리, 로그

--- cookiee-server/src/main/java/com/cookiee/cookieeserver/login/apple/controller/AppleController.java
+++ cookiee-server/src/main/java/com/cookiee/cookieeserver/login/apple/controller/AppleController.java
@@ -5,11 +5,12 @@ import com.cookiee.cookieeserver.global.dto.BaseResponseDto;
 import com.cookiee.cookieeserver.global.dto.DataResponseDto;
 import com.cookiee.cookieeserver.global.dto.ErrorResponseDto;
 import com.cookiee.cookieeserver.login.OAuthResponse;
+import com.cookiee.cookieeserver.login.apple.dto.response.AppleLoginResponse;
 import com.cookiee.cookieeserver.login.apple.service.AppleService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.cookiee.cookieeserver.global.Constant.*;
 
@@ -20,14 +21,26 @@ public class AppleController {
 
     // 로그인 성공 시 설정된 redirect url로 받는 authorization code를 service 단에 넘겨줌
     // 정상적으로 로그인 관련 처리가 끝났다면 관련 데이터를 클라이언트에 보내준다.
-    @PostMapping("/login/apple")
+    @PostMapping("/auth/login/apple")
+    @ResponseBody
     public BaseResponseDto<?> appleOAuthRequest(@RequestHeader(HEADER_IDENTITY_TOKEN) String idToken,
                                                 @RequestHeader(HEADER_APPLE_AUTHORIZATION_CODE) String authorizationCode) {
-        OAuthResponse response = appleService.login(idToken, authorizationCode);
-        // 애플 회원가입 또는 로그인 실패
-        if (response == null) {
-            return ErrorResponseDto.of(StatusCode.UNAUTHORIZED, "애플 로그인 실패");
+        try{
+            OAuthResponse response = appleService.login(idToken, authorizationCode);
+            return DataResponseDto.of(response, "애플 로그인에 성공하였습니다.");
         }
-        return DataResponseDto.of(response, "애플 로그인에 성공하였습니다.");
+        // 애플 회원가입 또는 로그인 실패
+        catch(Exception e){
+            return ErrorResponseDto.of(StatusCode.VALIDATION_ERROR, e.getMessage());
+        }
     }
+
+//    @RequestMapping("/login/apple/callback")
+//    @ResponseBody
+//    public AppleLoginResponse callback(HttpServletRequest request, HttpServletResponse response) {
+//        return AppleLoginResponse.builder()
+//                .code(request.getParameter("code"))
+//                .idToken(request.getParameter("id_token"))
+//                .build();
+//    }
 }
