93930961a0c690dea3f17bbf18220c12f17fb4c5
Suyeon <syhhwang1231@gmail.com>
2024-02-29 01:44:44 +0900

    Feat: 전역 예외 처리를 위한 코드
    - ErrorCode: 상수로 사용될 에러 선언해놓은 파일
    - ExceptionAdvice: 전역 예외 처리 핸들러
    - GeneralException: 커스텀한 exception

---
+++ cookiee-server/src/main/java/com/cookiee/cookieeserver/global/ErrorCode.java
@@ -0,0 +1,35 @@
+package com.cookiee.cookieeserver.global;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    // 일반적인 응답
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러, 문의 바랍니다"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+
+    // 인증 & 인가
+    // INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "INVALID INPUT VALUE"),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효한 토큰이 아닙니다."),
+    NULL_AUTHORIZATION_HEADER(HttpStatus.UNAUTHORIZED, "Authorization Header가 빈 값입니다."),
+    INVALID_HEADER_GRANT_TYPE(HttpStatus.UNAUTHORIZED, "Bearer 타입이 아닙니다."),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "해당 refresh token은 존재하지 않습니다."),
+    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "해당 refresh token은 만료되었습니다."),
+
+    // 유저
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "NO USER FOUND");
+
+    // 카테고리
+
+    // 이벤트
+
+    // 컬렉션
+
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
